0529ccd728db275c854b367ac6d954265efdc833
feven <46973832+fevent07@users.noreply.github.com>
2023-09-29 03:00:25 -0400

    Update Itemtemplates.py

--- event_management/Itemtemplates.py
+++ event_management/Itemtemplates.py
@@ -7,3 +7,11 @@ def get_quotation_templlat(name):
         doc = None
     return doc
     
+@frappe.whitelist()
+def get_quotation_templllaat(name):
+
+    try:
+        doc = frappe.get_doc('Sales order template', name)
+    except Exception as e:
+        doc = None
+    return doc 
